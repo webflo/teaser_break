b1582052f58e62c79a5e23bca2fb03e55a24f873
Florian Weber <florian.weber@ueberbit.de>
2016-02-19 11:58:06 +0100

    Add Fiedl Widget

---
+++ src/Plugin/Field/FieldWidget/TextareaWithTeaserBreak.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\teaser_break\Plugin\Field\FieldWidget\TextareaWithTeaserBreak.
+ */
+
+namespace Drupal\teaser_break\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\text\Plugin\Field\FieldWidget\TextareaWidget;
+
+/**
+ * Plugin implementation of the 'teaser_break_textarea' widget.
+ *
+ * @FieldWidget(
+ *   id = "teaser_break_textarea",
+ *   label = @Translation("Text area with a summary (Teaser-Break)"),
+ *   field_types = {
+ *     "text_with_summary"
+ *   }
+ * )
+ */
+class TextareaWithTeaserBreak extends TextareaWidget {
+
+  /**
+   * {@inheritdoc}
+   */
+  function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = parent::formElement($items, $delta, $element, $form, $form_state);
+    if (!empty($items[$delta]->summary)) {
+      $element['#default_value'] = $items[$delta]->summary . '<teaser-break></teaser-break>' . $element['#default_value'];
+    }
+    return $element;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
+    $values = parent::massageFormValues($values, $form, $form_state);
+    foreach ($values as &$value) {
+      $text = explode('<teaser-break></teaser-break>', $value['value']);
+      if (count($text) == 2) {
+        $value['summary'] = $text[0];
+        $value['value'] = $text[1];
+      }
+    }
+    return $values;
+  }
+
+}
